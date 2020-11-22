61f4a89352f98771dc3122363b60be1bc5b249aa
anggawanridho <anggawanridho@gmail.com>
2020-11-22 13:15:30 +0700

    first commit

---
+++ src/gradien_garis_lurus/GarisLurus.java
@@ -0,0 +1,11 @@
+package gradien_garis_lurus;
+
+/**
+ * @author NAMA     : Muhammad Anggawan Ridho Islami
+ * KELAS    : IF 10K
+ * NIM      : 10119909
+ */
+
+public interface GarisLurus {
+    public int hitungGradien();
+}
---
+++ src/gradien_garis_lurus/PBO10K10119909Latihan63.java
@@ -0,0 +1,18 @@
+package gradien_garis_lurus;
+
+
+
+public class PBO10K10119909Latihan63 {
+
+    public static void main(String[] args) {
+        Koordinat koor1 = new Koordinat(2,10,5,7);
+        int gradien1 = koor1.hitungGradien();
+        System.out.println("Garis yang melalui titik (" + koor1.getX1() + "," + koor1.getY1() + ") dan (" + koor1.getX2() + "," + koor1.getY2() + ")");
+        System.out.println("memiliki gradien sebesar " + gradien1);
+
+        Koordinat koor2 = new Koordinat(5,1,3,12);
+        int gradien2 = koor2.hitungGradien();
+        System.out.println("Garis yang melalui titik (" + koor2.getX1() + "," + koor2.getY1() + ") dan (" + koor2.getX2() + "," + koor2.getY2() + ")");
+        System.out.println("memiliki gradien sebesar " + gradien2);
+    }
+}
